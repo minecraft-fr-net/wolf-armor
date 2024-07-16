0f3ab06725e4a8780c1d3abdd199cf4f8ace2724
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-07-16 11:47:27 +0200

    🔊 Improve log

--- src/main/java/net/minecraftfr/wolfarmormod/WolfArmorMod.java
+++ src/main/java/net/minecraftfr/wolfarmormod/WolfArmorMod.java
@@ -14,11 +14,8 @@ public class WolfArmorMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Initialize WolfArmorMod");
 
-		LOGGER.info("Hello Fabric world!");
 		ModArmorMaterials.initialize();
 		ModItems.initialize();
 
