5e58e1f487514fd286e93ac713ab54349f9b767e
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-09-02 21:57:58 +0200

    ♻️ Refactor LootTable

--- src/main/java/net/minecraftfr/wolfarmor/WolfArmor.java
+++ src/main/java/net/minecraftfr/wolfarmor/WolfArmor.java
@@ -1,6 +1,14 @@
 package net.minecraftfr.wolfarmor;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.LootingEnchantLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +22,30 @@ public class WolfArmor implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initialize WolfArmor");
+		LOGGER.info("Initialize " + MOD_ID);
 
 		ModArmorMaterials.initialize();
 		ModItems.initialize();
 
-		WolfDropHandler.register();
+		LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+			// Vérification si la table de loot correspond à celle du loup
+			if (EntityType.WOLF.getLootTableId().equals(key)) {
+				// Création d'une pool de loot pour les os
+				LootPool leatherPool = LootPool.builder()
+								.with(ItemEntry.builder(ModItems.WOLF_LEATHER)
+												.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)))
+												.apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f))))
+								.build();
+
+				LootPool toothPool = LootPool.builder()
+								.with(ItemEntry.builder(ModItems.WOLF_TOOTH)
+												.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)))
+												.apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f))))
+								.build();
+
+				tableBuilder.pool(leatherPool);
+				tableBuilder.pool(toothPool);
+			}
+		});
 	}
 }
